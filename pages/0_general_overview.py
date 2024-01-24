45d4d880fc505331f5335f2b2873ce8daa15d444
victor-bongestab <menosmaisvezesdividido@gmail.com>
2024-01-24 18:36:38 -0300

    .

--- pages/0_general_overview.py
+++ pages/0_general_overview.py
@@ -5,9 +5,9 @@ import inflection
 import streamlit as st
 from PIL import Image
 
-#from streamlit_folium import folium_static
-#import folium
-#from folium.plugins import MarkerCluster
+from streamlit_folium import folium_static
+import folium
+from folium.plugins import MarkerCluster
 
 
 # Dataset
